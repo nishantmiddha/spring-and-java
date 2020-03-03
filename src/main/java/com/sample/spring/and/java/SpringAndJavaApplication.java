58d73e2098d162d833282dbc980936a69a9fedf8
NISHANT MIDDHA <nishantmiddha@Nishants-MacBook-Pro.local>
2020-03-03 22:46:02 +0530

    Committing base files........

---
+++ src/main/java/com/sample/spring/and/java/SpringAndJavaApplication.java
@@ -0,0 +1,13 @@
+package com.sample.spring.and.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAndJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAndJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/spring/and/java/SpringAndJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.spring.and.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAndJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
